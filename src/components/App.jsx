57ffdfa5a36c88ed126fea80b5df525a38f374f6
AShvab <shvabovska@gmail.com>
2023-05-05 22:31:04 +0300

    day first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { nanoid } from 'nanoid';
+import Form from './Form';
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = data =>{
+console.log(data)
+  };
+
+  render() {
+    return (
+      <>
+        <h1>PhoneBook</h1>
+        {/* onSubmit це проп, при сабміті отримуємо фінальні дані форми */}
+        <Form onSubmit={this.formSubmitHandler} />       
+      </>
+    );
+  }
+}
+
+export default App;
