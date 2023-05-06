abbbcb2d6e8a5e25b952a593e24dfbd64953c324
AShvab <shvabovska@gmail.com>
2023-05-06 22:58:23 +0300

    remove comments

--- src/components/SearchContact/SearchContact.jsx
+++ src/components/SearchContact/SearchContact.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import { nanoid } from 'nanoid';
-// import PropTypes from 'prop-types';
-
-// function SearchContact({searchContact }) {
-//   const filterID = nanoid();
-//   return (
-//     <div>
-//       <label>Find contacts by name</label>
-//         <input 
-//         id={filterID}
-//         type="text"
-//         name='filter'
-//         placeholder='Enter name'
-//         onChange={e => searchContact(e.target.value)}
-//         />
-     
-//     </div>
-//   )
-// }
-
-// SearchContact.prototype = {
-//     searchContact: PropTypes.func.isRequired,
-//   };
-
-// export default SearchContact;
 import React from 'react';
 import PropTypes from 'prop-types';
 
